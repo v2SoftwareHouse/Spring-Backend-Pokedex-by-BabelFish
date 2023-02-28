9a0a893da149037d073213101bb1763c5222e473
v2SoftwareHouse <v2.softwarehouse@gmail.com>
2023-02-28 14:16:02 -0300

    review PokedexByBabelFishApplicationTests

--- src/main/kotlin/com/v2/sample/babelfish/feature/landing/business/LandingRepository.kt
+++
@@ -1,5 +0,0 @@
-package com.v2.sample.babelfish.feature.landing.business
-
-interface LandingRepository {
-    fun doFetch(name:String?): Any?
-}
--- src/main/kotlin/com/v2/sample/babelfish/feature/landing/gateway/LandingPresenter.kt
+++
@@ -1,5 +0,0 @@
-package com.v2.sample.babelfish.feature.landing.gateway
-
-interface LandingPresenter {
-    fun doFetch(name: String?): Any?
-}
